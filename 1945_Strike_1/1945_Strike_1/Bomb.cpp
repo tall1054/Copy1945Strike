9561f1f6a1bba9bc7089a87e6238f65c85ab374c
tall1054@gmail.com <tall1054@gmail.com>
2022-08-19 17:20:11 +0900

    commit coment

--- 1945_Strike_1/1945_Strike_1/Bomb.cpp
+++ 1945_Strike_1/1945_Strike_1/Bomb.cpp
@@ -5,7 +5,7 @@
 #include "BmpMgr.h"
 #include "ObjMgr.h"
 
-//비행기 출현으로 모든 몬스터와 그 총알을 제거하는...
+//비행기 출현으로 모든 몬스터와 그 총알을 제거하는 방법입니다. 
 CBomb::CBomb()
 {
 }
@@ -50,15 +50,15 @@ void CBomb::Render(HDC hDC)
 	HDC	hMemDC = CBmpMgr::Get_Instance()->Find_Image(L"Bomb");
 
 	GdiTransparentBlt
-	(hDC,				//	복사 받을, 최종적으로 그림을 그릴 DC
-		int(m_tRect.left),	//	2, 3인자 복사 받을 위치의 좌표를 전달
+	(hDC,								//	복사 받을, 최종적으로 그림을 그릴 DC
+		int(m_tRect.left),				//	2, 3인자 복사 받을 위치의 좌표를 전달
 		int(m_tRect.top),
 		(int)m_tInfo.fCX,				//	4, 5인자 복사 받을 X,Y의 길이를 전달	
 		(int)m_tInfo.fCY,
 		hMemDC,							// 비트맵 정보를 가지고 있는 DC
 		0,								// 7,8인자 비트맵을 출력할 시작좌표
 		0,
-		450,								// 9, 10인자 비트맵의 X,Y의 길이를 전달
+		450,							// 9, 10인자 비트맵의 X,Y의 길이를 전달
 		450,
 		RGB(255, 255, 255));
 }
